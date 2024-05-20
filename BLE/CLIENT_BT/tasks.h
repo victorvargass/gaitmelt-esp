46f5b6dfc8622c69dba77a2ddaed5a6f02831f04
Franco <frank_kings@outlook.com>
2024-05-20 17:22:15 -0400

    Se añade la medición del retardo en la lectura de una estructura y el envío de un paquete en las funciones respectivas.

--- BLE/CLIENT_BT/tasks.h
+++ BLE/CLIENT_BT/tasks.h
@@ -9,12 +9,14 @@ void vTaskReadMPUData(void *pvParameters){
     // notify changed value
     if (deviceConnected) {
       if (structsRead < NUM_STRUCTS_TO_SEND) {
+        unsigned long startTime_mpuMeasure = micros();
         // Leer los datos de la MPU solo si no hemos leído suficientes estructuras aún
         readMPUData();
         // Almacenar los datos en el arreglo
         mpuReadings[structsRead] = thisMPUReadings;
         // Incrementar el contador de estructuras leídas
         structsRead++;
+        Serial.printf("\r\nMPU measure delay: [%lu]\r\n",micros()-startTime_mpuMeasure);
       }
       
     }
@@ -27,15 +29,16 @@ void vTaskBLEHandler(void *pvParameters){
       if(deviceConnected){
           // Solo enviar cuando hayamos leído suficientes estructuras
           if (structsRead >= NUM_STRUCTS_TO_SEND) {
+            unsigned long startTime_bleSend = micros();
             // Convertir estructuras a bytes y establecer el valor de la característica BLE
             uint8_t buffer[sizeof(struct_message_mpu) * NUM_STRUCTS_TO_SEND];
             packStructsToBytes(mpuReadings, buffer, NUM_STRUCTS_TO_SEND);
             pMPUCharacteristic->setValue(buffer, sizeof(buffer));
             pMPUCharacteristic->notify();
-
             // Reiniciar el contador de estructuras leídas
             structsRead = 0;
             delay(interval); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+            Serial.printf("\r\nBLE send delay: [%lu]\r\n",micros()-startTime_bleSend);
           }
       }
       // disconnecting
