0c26532422c0713c436712bf2ff75df2e6818126
Victor Vargas <victorvargassandoval93@gmail.com>
2024-08-20 18:47:47 -0400

    ADD: alternate change class and python script

---
+++ WIFI/python/alternate_change.py
@@ -0,0 +1,123 @@
+import tkinter as tk
+import queue
+import threading
+from utils import AlternateChange  # Asegúrate de que esta importación sea correcta
+
+# Configuración de los ESPs
+ESP_IPS = {
+    1: "192.168.50.11",
+    2: "192.168.50.12",
+    3: "192.168.50.13",
+    4: "192.168.50.14",
+}
+
+STRUCT_FORMAT = "i fff fff i"
+LOCAL_UDP_IP = "192.168.50.82"
+SHARED_UDP_PORT = 4210
+OUTPUT_FILENAME = "PGMLT_001"
+
+READING_MODE = True
+
+VIBRATION_CADENCE = 1000
+MOTOR_POWER = 250 # 70
+
+ESP_INDEXES = [1, 2, 3, 4]
+
+SCREEN_SIZE = "1300x900"
+
+# Crear una instancia de GaitMelt
+alternate_change = AlternateChange(
+    local_udp_ip=LOCAL_UDP_IP,
+    shared_port=SHARED_UDP_PORT,
+    esp_indexes=ESP_INDEXES,
+    esp_ips=ESP_IPS,
+    struct_format=STRUCT_FORMAT,
+    motor_power=MOTOR_POWER,
+    vibration_cadence=VIBRATION_CADENCE
+)
+
+# Configurar la interfaz gráfica
+root = tk.Tk()
+root.geometry(SCREEN_SIZE)
+root.option_add("*Font", "Helvetica 20")
+
+# Configurar el layout de la cuadrícula
+for i in range(4):
+    root.grid_columnconfigure(i, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+
+# Crear y ubicar los paneles
+label_texts = [tk.StringVar() for _ in range(4)]
+for i, text in enumerate(label_texts):
+    text.set(f"Board {i+1} no conectada")
+
+panels = [
+    tk.Label(
+        root,
+        textvariable=text,
+        padx=10,
+        pady=10,
+        borderwidth=2,
+        relief="solid",
+        width=50,
+        height=20,
+    )
+    for text in label_texts
+]
+
+for i, panel in enumerate(panels):
+    row = i // 2
+    col = i % 2
+    panel.grid(row=row, column=col, padx=10, pady=10)
+
+label_output_filename = tk.Label(root, text="Nombre del archivo")
+label_output_filename.grid(row=2, column=0, columnspan=2, pady=(20, 0))
+input_output_filename = tk.Entry(root)
+input_output_filename.grid(row=3, column=0, columnspan=2, pady=(20, 0))
+input_output_filename.insert(0, OUTPUT_FILENAME)  # Establecer el valor por defecto
+input_output_filename.bind('<KeyRelease>', alternate_change.update_output_filename)
+
+
+init_button = tk.Button(
+    root,
+    text="Iniciar",
+    command=lambda: alternate_change.toggle_alternative_vibration(init_button),
+    bg="green",
+    fg="white",
+)
+init_button.grid(row=2, column=1, columnspan=2, pady=(20, 0))
+
+vc_slider_label = tk.Label(root, text="Cadencia vibración [ms]")
+vc_slider_label.grid(row=4, column=1, columnspan=4, pady=(20, 0))
+
+vc_slider = tk.Scale(
+    root,
+    from_=500,
+    resolution=100,
+    to=2000,
+    orient="horizontal",
+    length=200,
+    command=lambda value: alternate_change.update_vc(vc_slider.get()),
+)
+vc_slider.set(alternate_change.vibration_cadence)
+vc_slider.grid(row=5, column=1, columnspan=2)
+
+# Configurar threads para la recepción de datos y actualización de la GUI
+esp_data = [None] * 4
+data_queue = queue.Queue()
+
+receive_thread = threading.Thread(
+    target=alternate_change.receive_data, args=(data_queue, esp_data)
+)
+receive_thread.daemon = True
+receive_thread.start()
+
+update_thread = threading.Thread(
+    target=alternate_change.update_gui,
+    args=(data_queue, label_texts, root),
+)
+update_thread.daemon = True
+update_thread.start()
+
+root.mainloop()
--- WIFI/python/utils.py
+++ WIFI/python/utils.py
@@ -9,6 +9,241 @@ import os
 import socket
 import json
 
+class AlternateChange:
+    def __init__(
+        self,
+        local_udp_ip,
+        shared_port,
+        esp_indexes,
+        esp_ips,
+        struct_format,
+        motor_power,
+        vibration_cadence
+    ):
+        self.local_udp_ip = local_udp_ip
+        self.shared_port = shared_port
+        self.esp_indexes = esp_indexes
+        self.esp_ips = esp_ips
+        self.struct_format = struct_format
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.motor_power = motor_power
+        self.vibration_cadence = vibration_cadence
+        self.max_time_sync_diff = 8  # Máxima diferencia de tiempo permitida (8 ms)
+
+        # Estado de grabación
+        self.alternate_vibrating = False
+        self.start_time = None
+        self.recorded_data = []
+        self.buffers = [[] for _ in range(4)]
+        self.last_vibration_ts = [0 for _ in range(4)]
+        self.vibrating = [False for _ in range(4)]
+        # self.sock = self.setup_socket(local_udp_ip, shared_port)
+
+        self.vd = 100
+
+    def setup_socket(self, local_ip, shared_port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((local_ip, shared_port))
+        return sock
+
+    def update_output_filename(self, event):
+        new_output_filename = event.widget.get()
+        self.output_filename = new_output_filename
+
+    def update_vc(self, new_vc):
+        self.vibration_cadence = int(new_vc)
+        self.set_selected_motors_vibration_time()
+
+    def alternate_vibrate(self, esp_id):
+        current_ts = time.time()
+        if (
+            esp_id == 1
+            and (current_ts - self.last_vibration_ts[0]) * 1000 <= self.vd
+            and not self.vibrating[0]
+        ):
+            self.vibrating[0] = True
+            print("Vibrando... 1 y 4")
+        if (
+            esp_id == 2
+            and (current_ts - self.last_vibration_ts[1]) * 1000 <= self.vd
+            and not self.vibrating[1]
+        ):
+            self.vibrating[1] = True
+            print("Vibrando... 2 y 3")
+
+        if (
+            esp_id == 1
+            and (current_ts - self.last_vibration_ts[0]) * 1000 <= self.vd
+            and self.vibrating[0]
+        ):
+            self.vibrating[0] = False
+            self.vibrating[3] = False
+            print("Dejó de vibrar... 1 y 4")
+        if (
+            esp_id == 2
+            and (current_ts - self.last_vibration_ts[1]) * 1000 <= self.vd
+            and self.vibrating[1]
+        ):
+            self.vibrating[1] = False
+            self.vibrating[2] = False
+            print("Dejó de vibrar... 2 y 3")
+
+        if esp_id == 1 and self.last_vibration_ts[0] == 0:  # primera vibracion
+            print("Primera vibracion 1 y 4")
+            self.last_vibration_ts[0] = current_ts
+            self.last_vibration_ts[1] = current_ts - self.vibration_cadence
+            self.vibrating[0] = True
+            self.activate_selected_motors([1, 4])
+
+        else:
+            if (
+                esp_id == 1
+                and self.last_vibration_ts[0] != 0
+                and not self.vibrating[0]
+                and current_ts - self.last_vibration_ts[0] > self.vibration_cadence
+            ):
+                print("Vibracion 1 y 4")
+                self.last_vibration_ts[0] = current_ts
+                self.vibrating[0] = True
+                self.activate_selected_motors([1, 4])
+
+            if (
+                esp_id == 2
+                and self.last_vibration_ts[1] != 0
+                and not self.vibrating[1]
+                and current_ts - self.last_vibration_ts[1] > self.vibration_cadence
+            ):
+                print("Vibracion 2 y 3")
+                self.last_vibration_ts[1] = current_ts
+                self.vibrating[1] = True
+                self.activate_selected_motors([2, 3])
+
+    def update_data(self, data, label_texts):
+        if self.alternate_vibrating:
+            if self.start_time is None:
+                self.start_time = time.time()
+            elapsed_time = time.time() - self.start_time
+
+            for esp_id in self.esp_indexes:
+                if data[esp_id - 1] is not None:
+                    self.buffers[esp_id - 1].append(data[esp_id - 1])
+
+            while all(self.buffers):
+                tss = [self.buffers[esp_id - 1][0][7] for esp_id in self.esp_indexes]
+                min_ts = min(tss)
+                max_ts = max(tss)
+
+                if max_ts - min_ts <= self.max_time_sync_diff:
+                    record_entry = [elapsed_time]
+                    for esp_id in self.esp_indexes:
+                        synchronized_data = self.buffers[esp_id - 1].pop(0)
+                        record_entry.extend(
+                            [
+                                synchronized_data[7],
+                                synchronized_data[1],
+                                synchronized_data[2],
+                                synchronized_data[3],
+                                synchronized_data[4],
+                                synchronized_data[5],
+                                synchronized_data[6],
+                            ]
+                        )
+                        self.alternate_vibrate(esp_id)
+                    self.recorded_data.append(record_entry)
+                else:
+                    oldest_index = tss.index(min_ts)
+                    self.buffers[oldest_index].pop(0)
+
+        for esp_id in self.esp_indexes:
+            if data[esp_id - 1] is not None:
+                board_id = data[esp_id - 1][0]
+                label_texts[esp_id - 1].set(
+                    f"Board ID: {board_id}\n"
+                    f"Acc X: {round(data[esp_id - 1][1], 3)}\n"
+                    f"Acc Y: {round(data[esp_id - 1][2], 3)}\n"
+                    f"Acc Z: {round(data[esp_id - 1][3], 3)}\n"
+                    f"Gyr X: {round(data[esp_id - 1][4], 3)}\n"
+                    f"Gyr Y: {round(data[esp_id - 1][5], 3)}\n"
+                    f"Gyr Z: {round(data[esp_id - 1][6], 3)}\n"
+                    f"Timestamp: {data[esp_id - 1][7]}"
+                )
+            else:
+                label_texts[esp_id - 1].set(f"Board {esp_id} no conectada")
+
+    def receive_data(self, data_queue, esp_data):
+        while True:
+            data, _ = self.sock.recvfrom(1024)
+            if len(data) == struct.calcsize(self.struct_format):
+                mpu_readings = struct.unpack(self.struct_format, data)
+                esp_id = mpu_readings[0]
+                esp_data[esp_id - 1] = mpu_readings
+                data_queue.put(esp_data.copy())
+
+    def update_gui(self, data_queue, label_texts, root):
+        while True:
+            if not data_queue.empty():
+                data = data_queue.get()
+                root.after(
+                    0,
+                    self.update_data,
+                    data,
+                    label_texts,
+                )
+
+    def reinitialize_gaitmelt_variables(self):
+        self.alternate_vibrating = False
+        self.start_time = None
+        self.recorded_data = []
+        self.buffers = [[] for _ in range(4)]
+        self.last_vibration_ts = [0 for _ in range(4)]
+        self.vibrating = [False for _ in range(4)]
+
+    def init_alternate_vibration(self, init_button):
+        self.reinitialize_gaitmelt_variables()
+        init_button.config(text="Iniciar", bg="green", fg="white")
+
+    def stop_alternate_vibration(self, init_button):
+        self.start_time = None
+        self.recorded_data = []
+        init_button.config(text="Detener", bg="red", fg="white")
+
+    def toggle_alternative_vibration(self, init_button):
+        self.alternate_vibrating = not self.alternate_vibrating
+        if self.alternate_vibrating:
+            self.stop_alternate_vibration(init_button)
+        else:
+            self.init_alternate_vibration(init_button)
+
+    def send_esp_message(self, IP, message):
+        try:
+            self.sock.sendto(message.encode(), (IP, self.shared_port))
+        except Exception as e:
+            print(f"Error sending message {message} to {IP}: {e}")
+
+    def activate_selected_motors(self, selected_esp_indexes):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    self.send_esp_message,
+                    self.esp_ips[esp],
+                    "motor" + str(self.vibration_offset),
+                )
+                for esp in selected_esp_indexes
+            ]
+            concurrent.futures.wait(futures)
+
+    def set_selected_motors_vibration_time(self):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    self.send_esp_message,
+                    self.esp_ips[esp],
+                    "duration" + str(self.vd),
+                )
+                for esp in self.esp_indexes
+            ]
+            concurrent.futures.wait(futures)
+
 
 class GaitMelt:
     def __init__(
@@ -55,7 +290,7 @@ class GaitMelt:
         self.mark_times_2 = []
         self.vibration_times = [[] for _ in range(num_esps)]
         self.buffers = [[] for _ in range(num_esps)]
-        self.sock = self.setup_socket(local_udp_ip, shared_port)
+        #self.sock = self.setup_socket(local_udp_ip, shared_port)
 
         # Variables caminata
         self.esp_steps = []  # Lista de indices para saber que esp tocó talon
